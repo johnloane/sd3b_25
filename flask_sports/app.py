5a16ba1994e6c4c778056d2e62ea8af02cdbfe20
johnloane <john.loane@dkit.ie>
2025-10-03 13:08:12 +0100

    Add DB support
    Can create and delete records in the db
    Need to pip install flask-mysqldb

--- flask_sports/app.py
+++ flask_sports/app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+import os
+from dotenv import load_dotenv, find_dotenv
 
 app = Flask(__name__)
 
+_ = load_dotenv(find_dotenv())
+app.config['MYSQL_HOST'] = os.environ.get("MYSQL_HOST")
+app.config['MYSQL_USER'] = os.environ.get("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.environ.get("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = os.environ.get("MYSQL_DB")
+
+mysql = MySQL(app)
+
+
+
 SPORTS = ["Fencing", "Airsoft"]
 
-REGISTRANTS = {}
 
 @app.route("/")
 def index():
@@ -21,10 +33,35 @@ def register():
         return render_template("failure.html", message="You did not enter a sport")
     if sport not in SPORTS:
         return render_template("failure.html", message="Please do not try to hack our html")
-    REGISTRANTS[name] = sport
+    
+    cursor = mysql.connection.cursor()
+    # Check is this name and sport already exists in the database
+    # If it already exists don't add it again
+    cursor.execute("select * from registrant where name=%s and sport=%s", (name, sport))
+    rv = cursor.fetchall()
+    print(len(rv))
+    if(len(rv) == 0):
+        cursor.execute("insert ignore into registrant(name, sport) values(%s, %s)", (name, sport))
+        mysql.connection.commit()
+    else:
+        print("That name and sport are already registered")
     return redirect("/registrants")
 
 
 @app.route("/registrants")
 def registrants():
-    return render_template("registrants.html", registrants=REGISTRANTS)
+    cursor = mysql.connection.cursor()
+    cursor.execute("select * from registrant")
+    rv = cursor.fetchall()
+    return render_template("registrants.html", registrants = rv)
+
+
+@app.route("/deregister", methods=["POST"])
+def deregister():
+    id = request.form.get("id")
+    if id:
+        cursor = mysql.connection.cursor()
+        cursor.execute("delete from registrant where id=%s", id)
+        mysql.connection.commit()
+    return redirect("/registrants")
+    
